b6e5073cebe73c82ad2355939d2722174e8ad752
Jung2022 <quuu9999@gmail.com>
2022-06-05 19:47:45 +0900

    안드로이드-스프링 연결 완료

--- Flogging/app/src/main/java/com/project/flogging/home/HomeFragment.kt
+++ Flogging/app/src/main/java/com/project/flogging/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.project.flogging.R
+import com.project.flogging.SpringApplication
 import com.project.flogging.databinding.FragmentHomeBinding
+import com.project.flogging.model.MessageModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
@@ -15,6 +19,21 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding= FragmentHomeBinding.inflate(inflater, container, false)
+        val networkService = (activity?.applicationContext as SpringApplication).networkService
+        val testCall = networkService.doTest()
+
+        testCall.enqueue(object : Callback<MessageModel> {
+            override fun onResponse(call: Call<MessageModel>, response: Response<MessageModel>) {
+                binding.testTV.setText(response.body()?.test)
+            }
+
+            override fun onFailure(call: Call<MessageModel>, t: Throwable) {
+                call.cancel()
+            }
+
+
+        })
+
         return binding.root
     }
 }
---
+++ Flogging/app/src/main/java/com/project/flogging/model/MessageModel.kt
@@ -0,0 +1,5 @@
+package com.project.flogging.model
+
+data class MessageModel(
+    var test:String
+)
